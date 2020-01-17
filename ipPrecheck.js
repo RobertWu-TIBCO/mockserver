1f11baa6bffdb3839f68c82a21153aa5b3ed64bd
RobertWu-TIBCO <yawu@tibco-support.com>
2020-01-17 16:01:35 +0800

    :bug: fix ip precheck

--- ipPrecheck.js
+++ ipPrecheck.js
@@ -1,11 +1,12 @@
 const {all_interfaces} = require('./functions');
 
 console.dir(all_interfaces);
-console.dir(all_interfaces.eth0[0]);
-console.log(`your eth0 ip : ${all_interfaces.eth0[0].address}`);
+const eth0ip=all_interfaces.eth0 && all_interfaces.eth0[0] && all_interfaces.eth0[0].address ;
+console.log(`your eth0 ip : ${eth0ip}`);
 
+const wlanip = all_interfaces.WLAN && all_interfaces.WLAN[1] && all_interfaces.WLAN[1].address;
 const showWlanIp = () => {
-	const ip = all_interfaces.eth0 && all_interfaces.eth0[0] && all_interfaces.eth0[0].address || (all_interfaces.WLAN && all_interfaces.WLAN[1] && all_interfaces.WLAN[1].address);
+	const ip = eth0ip || wlanip;
 	console.log(`your lan ip is: ${ip}`);
 	return ip;
 };
